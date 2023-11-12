7bb51a3875fcadd198f6fe2e3d12be05e4b502b2
anatonokao <anato.devs@gmail.com>
2023-11-13 01:09:35 +0300

    feat: add nothing found test, add header render test, add test for card count

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,14 +1,46 @@
-import { render } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import App from './App';
-import { describe, it } from 'vitest';
+import { afterEach, describe, it } from 'vitest';
 import { MemoryRouter } from 'react-router-dom';
+import { AppContext } from './contexts/AppContext/AppContextProvider';
 
 describe('Main component', () => {
-  it('check render', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('App component rendered', () => {
     render(
       <MemoryRouter>
         <App />
       </MemoryRouter>
     );
+  });
+
+  it('Nothing found is displayed if no cards are present', async () => {
+    const value = {
+      request: 'book',
+      setRequest: vi.fn(),
+      response: {
+        kind: 'books#volumes',
+        totalItems: 0,
+        items: [],
+      },
+      setResponse: vi.fn(),
+    };
+
+    await render(
+      <MemoryRouter>
+        <AppContext.Provider value={value}>
+          <App />
+        </AppContext.Provider>
+      </MemoryRouter>
+    );
+
+    // const notFoundText = screen.findByTestId('nothing-found');
+
+    await expect(
+      await screen.findByTestId('nothing-found')
+    ).toBeInTheDocument();
   });
 });
