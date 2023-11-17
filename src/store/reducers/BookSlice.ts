c84d9898c44a40df2bd8fdd167e02103a61da67b
anatonokao <anato.devs@gmail.com>
2023-11-17 06:58:35 +0300

    feat: add async thunk, add reducers for fetch books

--- src/store/reducers/BookSlice.ts
+++ src/store/reducers/BookSlice.ts
@@ -1,5 +1,6 @@
 import { IBook } from '../../models/IBook';
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchBooks } from './ActionCreators';
 
 interface BookState {
   books: IBook[];
@@ -17,6 +18,35 @@ export const bookSlice = createSlice({
   name: 'book',
   initialState: initState,
   reducers: {},
+  // extraReducers: {
+  //   [fetchBooks.pending.type]: (state) => (state.isLoading = true),
+  //   [fetchBooks.fulfilled.type]: (state, action: PayloadAction<IBook[]>) => {
+  //     state.isLoading = false;
+  //     state.error = '';
+  //     state.books = action.payload;
+  //   },
+  //   [fetchBooks.rejected.type]: (state, action: PayloadAction<string>) => {
+  //     state.isLoading = false;
+  //     state.error = action.payload;
+  //   },
+  // },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = '';
+        state.books = action.payload.items;
+      })
+      .addCase(fetchBooks.rejected, (state, action) => {
+        state.isLoading = false;
+        console.log(action);
+        state.error =
+          typeof action.payload === 'string' ? action.payload : 'Error 404';
+      });
+  },
 });
 
 export default bookSlice.reducer;
